21055ef9983f8284abd649a5a0f83a789399c050
Marianecamposcordeiro <campos.cordeiro.mariane@escola.pr.gov.br>
2025-08-14 08:30:38 -0300

    hhhhh

---
+++ pergunta.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'História',
+    'O que foi o feudalismo?',
+    'Um sistema social e político predominante na Europa medieval.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'Química',
+    'O que é química?',
+    'A química é a ciência que estuda a matéria, suas propriedades, composição e transformações.'
+)
+    criaCartao(
+    'Matemática',
+    'Qual é o resultado de 15 + 7 - 3 x 2?',
+    '18'
+)
